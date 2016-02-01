6352d7b656b7495aa24d0762912b5f31e1c60b32
Kévin Roussel <Kevin.Roussel@inria.fr>
2016-02-01 16:05:22 +0100

    Ajout de la bibliographie nécessaire à la section 5 ("Consequences")

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 pdflatex Pres-KR-EWSN2016-NextMote.tex
-#pdflatex Pres-KR-EWSN2016-NextMote.tex
+bibtex Pres-KR-EWSN2016-NextMote
+pdflatex Pres-KR-EWSN2016-NextMote.tex
 pdflatex Pres-KR-EWSN2016-NextMote.tex
